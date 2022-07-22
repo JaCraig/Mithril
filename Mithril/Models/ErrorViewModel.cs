d98d0d1cf1ab405e68a127136459b6f674e11e42
James Craig <craig.jamesa@gmail.com>
2022-07-22 18:48:30 -0400

    - Initial push.

---
+++ Mithril/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mithril.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
