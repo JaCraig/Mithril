9b2d148b5e23085411a92016a93613c2ec008f22
James Craig <craig.jamesa@gmail.com>
2022-07-24 23:20:44 -0400

    - Just setting up editor settings.

---
+++ Mithril.Core/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Service collection extensions.
+    /// </summary>
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Setups Mithril services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <returns>The services object.</returns>
+        public static IServiceCollection SetupMithril(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- Mithril/Program.cs
+++ Mithril/Program.cs
@@ -4,29 +4,31 @@ namespace Mithril
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            _ = builder.Services.AddControllersWithViews();
+            _ = builder.Services.SetupMithril();
 
-            var app = builder.Build();
+            WebApplication? app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                _ = app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production
+                // scenarios, see https://aka.ms/aspnetcore-hsts.
+                _ = app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            _ = app.UseHttpsRedirection();
+            _ = app.UseStaticFiles();
 
-            app.UseRouting();
+            _ = app.UseRouting();
 
-            app.UseAuthorization();
+            _ = app.UseAuthorization();
 
-            app.MapControllerRoute(
+            _ = app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
